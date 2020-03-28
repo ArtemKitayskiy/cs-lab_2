5ef78d1837c759123db2e2166ab48d47e3292f13
Bob (KitayskiyAS) <bob@example.com>
2020-03-28 17:18:57 +0300

    code: min

--- main.cpp
+++ main.cpp
@@ -5,12 +5,20 @@ using namespace std;
 int main()
 {
     cout << "Enter A and B: ";
+<<<<<<< HEAD
     int a, b, max;
+=======
+    int a, b, min;
+>>>>>>> b01916a... code: min
     cin >> a >> b;
     cout << "A + B = " << a + b << '\n'
          << "A - B = " << a - b << '\n'
          << "A * B = " << a * b << '\n'
          << "A / B = " << a * b << '\n'
+<<<<<<< HEAD
          << "max = " << max << '\n';
+=======
+         << "min = " << min << '\n';
+>>>>>>> b01916a... code: min
     return 0;
 }
